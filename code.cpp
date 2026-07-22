54db66aeef223f8187f2c9d5592141e477df0b5f
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-07-22 03:46:12 -0500

    add folder

---
+++ code.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+//      cout << "Hello, World!" << endl;
+//      cout<< "Enter your name and age: " << endl;
+//    string name;
+//     int age;
+//    cin>> name;
+//    cin>>age; 
+//    cout<< "My name is : " <<name<<endl;
+//    cout<< "My age is : " <<age<<endl;
+
+int number;
+cout<< "Enter a number: " << endl;
+cin>> number;
+if(number%2==0)
+    cout<< number<< " is an even number." << endl;
+
+else     
+    cout<< number<< " is an odd number." << endl;
+    return 0;
+}
+
+
+
