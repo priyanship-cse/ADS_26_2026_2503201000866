6bf1335636a68689aabc7cd3aa2646bea7a47254
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-08-17 09:25:33 +0530

    add code

--- OOPS Lab/class and object/pi.cpp
+++ OOPS Lab/class and object/pi.cpp
@@ -1,77 +1,123 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
 
-class student{
-private:
-    string name;
-    int rollno;
-    float marks;
+// class student{
+// private:
+//     string name;
+//     int rollno;
+//     float marks;
 
-public:
-    void getdata()
-    {
-        cout<<"Enter Student name: ";
-        cin>>name;
+// public:
+//     void getdata()
+//     {
+//         cout<<"Enter Student name: ";
+//         cin>>name;
 
-        cout<<"Enter rollno: ";
-        cin>>rollno;
+//         cout<<"Enter rollno: ";
+//         cin>>rollno;
 
-        cout<<"Enter marks: ";
-        cin>>marks;
-
-    if(marks<0 || marks>100)
-    {
-        cout<<"Invalid marks entered. Please enter marks between 0 and 100."<<endl;
-        exit(1);
-
-    };
-
-    if(marks>=90)
-    {
-        cout<<"Grade: A"<<endl;
-    }
-    else if(marks>=80)
-    {
-        cout<<"Grade: B"<<endl;
-    }
-    else if(marks>=70)
-    {
-        cout<<"Grade: C"<<endl;
-    }
-    else if(marks>=60)
-    {
-        cout<<"Grade: D"<<endl;
-    }
-    else
-    {
-        cout<<"Grade: F"<<endl;
-    }
-
-    }
+//         cout<<"Enter marks: ";
+//         cin>>marks;
 
 
+//     if(marks<0 || marks>100)
+//     {
+//         cout<<"Invalid marks entered. Please enter marks between 0 and 100."<<endl;
+//         exit(1);
+
+//     };
+
+//     int grade;
+
+//     if(marks>=90)
+//     {
+//         cout<<"Grade: A"<<endl;
+//     }
+//     else if(marks>=80)
+//     {
+//         cout<<"Grade: B"<<endl;
+//     }
+//     else if(marks>=70)
+//     {
+//         cout<<"Grade: C"<<endl;
+//     }
+//     else if(marks>=60)
+//     {
+//         cout<<"Grade: D"<<endl;
+//     }
+//     else
+//     {
+//         cout<<"Grade: F"<<endl;
+//     }
+
+//     }
 
 
-    void displaydata()
+
+
+//     void displaydata()
 
 
          
-    {
-        cout<<"Name: "<<name<<endl;
-        cout<<"Rollno: "<<rollno<<endl;
-        cout<<"Marks: "<<marks<<endl;
-    }
-};
+//     {
+//         cout<<"Name: "<<name<<endl;
+//         cout<<"Rollno: "<<rollno<<endl;
+//         cout<<"Marks: "<<marks<<endl;
+//         cout<<"Grade: "<<grade<<endl;
+//     }
+// };
 
-  int main()
+//   int main()
 
-{
+// {
   
-    student p;
+//     student p;
 
-    p.getdata();
-     cout<< "====== Student Details======"<<endl;
-    p.displaydata();
-    return 0;
-}
+//     p.getdata();
+//      cout<< "====== Student Details======"<<endl;
+//     p.displaydata();
+//     return 0;
+// }
+
+
+//  // auto keyword is used to automatically deduce the type of a variable from its initializer.
+//  // It allows the compiler to determine the type of the variable based on the value assigned to it, 
+//  // making code more concise and easier to read.  # code 
+
+ 
+
+// 1	#include <iostream>
+// 2	using namespace std;
+// 3	
+// 4	int main() {
+// 5	 auto count = 5;
+// 6	 auto price = 48.67;
+// 7	 auto grade= 'A';
+// 8	 auto ready  = true;
+// 9	 
+// 10	 cout <<count<<' '<<price<<' '<<grade<<' '<<ready;
+// 11	 
+// 12	 
+// 13	  return 0;
+// 14	}
+
+
+// Stack
+// main
+// count	
+// int
+// 5
+// price	
+// double
+// 48.67
+// grade	
+// char
+// 'A'
+// ready	
+// bool
+// 1
+// Heap
+// C/C++ details: 
+// none [default view]
+
