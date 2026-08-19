f7852b81eb139947b8bdb69ea59e37bd4c9c54d3
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-08-19 09:50:24 +0530

    add code

--- DSA/Queue/deque.cpp
+++ DSA/Queue/deque.cpp
@@ -2,24 +2,90 @@
 using namespace std;
 #include <deque>
 
-int main () {
+// int main () {
    
-    deque<int> dq;
+//     deque<int> dq;
 
-dq.push_back(19);
-dq.push_back(29);
-dq.push_front(9);
+// dq.push_back(19);
+// dq.push_back(29);
+// dq.push_front(9);
 
-cout<<dq.front()<<endl;
-cout<<dq.back()<<endl;
-dq.pop_front();
-dq.pop_back();
-cout<<"Remaining element :  ";
+// cout<<dq.front()<<endl;
+// cout<<dq.back()<<endl;
+// dq.pop_front();
+// dq.pop_back();
+// cout<<"Remaining element :  ";
 
 
-for(int x:dq){
-    cout<<x<<endl;
+// for(int x:dq){
+//     cout<<x<<endl;
+// }
+
+
+// }
+
+// Insert/delete dynamic implementation
+
+// struct Node {
+//     int info;
+//     Node *next;
+//     Node *prev;
+
+// };
+
+// Node *front = NULL;
+// Node *rear = NULL;
+
+// void dq_insert(int x){
+//     Node *newNode = new Node();
+//     newNode->info = x;
+//     newNode->next = front;
+//     newNode->prev = NULL;
+
+//     if(front == NULL){
+//         front = newNode;
+//         rear = newNode;
+//     }
+    // else{
+    //     newNode->prev = NULL;
+    //     newNode->next = front;
+    //     front->prev = newNode;
+    //     front = newNode;
+    // }
+
+//     else{
+//         newNode->next = NULL;
+//         newNode->prev = rear;
+//         rear->next = newNode;
+//         rear = newNode;
+//     }
+
+// }
+
+void display() {
+    Node *temp = front;
+
+    if (front == NULL) {
+        cout << "Deque is empty";
+        return;
+    }
+
+    while (temp != NULL) {
+        cout << temp->info << " ";
+        temp = temp->next;
+    }
 }
 
+// int main() {
+//     dq_insert(10);
+//     dq_insert(20);
+//     dq_insert(30);
 
-}
+//     cout << "Elements in the deque: ";
+//     display();
+//     dq_insert(40);
+//     cout << "\nElements in the deque after inserting 40 at rear : ";
+//    display();
+//     return 0;
+// }
+
