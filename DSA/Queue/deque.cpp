398238cb13508323f3e8b9a7864074352550daf1
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-08-19 10:07:05 +0530

    add call back function

--- DSA/Queue/deque.cpp
+++ DSA/Queue/deque.cpp
@@ -26,41 +26,70 @@ using namespace std;
 
 // Insert/delete dynamic implementation
 
-// struct Node {
-//     int info;
-//     Node *next;
-//     Node *prev;
+struct Node {
+    int info;
+    Node *next;
+    Node *prev;
 
-// };
+};
 
-// Node *front = NULL;
-// Node *rear = NULL;
+Node *front = NULL;
+Node *rear = NULL;
 
-// void dq_insert(int x){
-//     Node *newNode = new Node();
-//     newNode->info = x;
-//     newNode->next = front;
-//     newNode->prev = NULL;
+void dq_insert(int x){
+    Node *newNode = new Node();
+    newNode->info = x;
+    newNode->next = front;
+    newNode->prev = NULL;
 
-//     if(front == NULL){
-//         front = newNode;
-//         rear = newNode;
-//     }
+    if(front == NULL){
+        front = newNode;
+        rear = newNode;
+    }
+    else{
+        newNode->prev = NULL;
+        newNode->next = front;
+        front->prev = newNode;
+        front = newNode;
+    }
+    // inserting at rear
     // else{
-    //     newNode->prev = NULL;
-    //     newNode->next = front;
-    //     front->prev = newNode;
-    //     front = newNode;
+    //     newNode->next = NULL;
+    //     newNode->prev = rear;
+    //     rear->next = newNode;
+    //     rear = newNode;
     // }
 
-//     else{
-//         newNode->next = NULL;
-//         newNode->prev = rear;
-//         rear->next = newNode;
-//         rear = newNode;
-//     }
+ }
 
-// }
+ // delete from front and rear
+
+ void dq_delete () {
+    Node *temp = front;
+    //Node *temp = rear;
+
+    if (front == NULL) 
+        cout << "Deque is empty";
+    else if(front == rear) {
+        front = NULL;
+        rear = NULL;
+        delete temp;
+    }
+    else {
+        front = front->next;
+        front->prev = NULL;
+        delete temp;
+    }
+
+    // rear ka liya 
+
+    // else {
+    //     rear = rear->prev;
+    //     rear->next = NULL;
+    //     delete temp;
+    // }
+    
+ }
 
 void display() {
     Node *temp = front;
@@ -75,17 +104,24 @@ void display() {
         temp = temp->next;
     }
 }
+ 
+int main() {
+    dq_insert(10);
+    dq_insert(20);
+    dq_insert(30);
 
-// int main() {
-//     dq_insert(10);
-//     dq_insert(20);
-//     dq_insert(30);
+    cout << "Elements in the deque: ";
+    display();
+    dq_insert(40);
+    cout << "\nElements in the deque after inserting 40 at front : ";
+   display();
+   dq_delete();
 
-//     cout << "Elements in the deque: ";
-//     display();
-//     dq_insert(40);
-//     cout << "\nElements in the deque after inserting 40 at rear : ";
-//    display();
-//     return 0;
-// }
+   cout << "\nElements in the deque after deleting from front : ";
+    display();
+    return 0;
+}
 
+
+ 
+   
