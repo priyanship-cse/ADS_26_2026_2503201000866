b2d309523b8e5e758ac3cf296f7154f090199f50
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-08-19 12:46:04 +0530

    add code

--- DSA/lab work/recursion.cpp
+++ DSA/lab work/recursion.cpp
@@ -8,11 +8,23 @@ int fact(int n){
     return 1;
     else
     return n*fact(n-1);
-    
 }
+
+int fib(int n){
+    if(n<=1)
+        return n;
+    else
+        return fib(n-1) + fib(n-2);
+}
+
+
 
 int main(){
     cout<<fact(5)<<endl;
+    cout<<fib(5)<<endl;
 };
+
+
+
 
 
