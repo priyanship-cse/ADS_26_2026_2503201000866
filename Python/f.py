21f9aeeda7d38b2b4822cbe9f31649fce091f5f6
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-08-20 09:16:53 +0530

    add file

--- Python/f.py
+++ Python/f.py
@@ -7,3 +7,46 @@ print(x is z )
 print(x is y )
 print(x == y )
 
+# Creating a dictionary
+
+thisdict = { 
+    "brand": "Ford",
+    "model": "Mustang",
+    "year1": 1994,
+    "color": "red",
+    "year" : 1994,
+
+}
+
+print(thisdict);
+print("Length of this dictionary is : " ,(len(thisdict)) );
+
+    
+student = {
+    "name" : "Prashant Tomar",
+    "age" : 31,
+}
+
+student.update(
+   
+   {"city" : "Baraut"}
+
+
+
+    )
+
+print(student); 
+
+# del delete last element of dictionary
+del student["age"]
+
+print(student);
+
+# clear delete all elements of dictionary
+student.clear()
+
+
+# if else condition
+
+
+
