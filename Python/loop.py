545d29c57ee37707baaab52ee3d320ac22942018
priyanshi_cse <priyanshipanwar348@gmail.com>
2026-08-21 14:36:53 +0530

    add loops code

---
+++ Python/loop.py
@@ -0,0 +1,26 @@
+i = 1
+while i < 6 :
+    print(i)
+    i += 1
+
+fruits = {"apple" , "grapes" , "banana" , "cherry"}
+for x in fruits:
+   print(x)
+
+   if x == "banana":
+     break  
+
+# range
+
+for x in range(10):
+   print(x)
+
+# condition range 
+
+for x in range  (2,8):
+    print(x)
+
+# range start end and last gap 
+
+for x in range ( 2,10,2):
+    print(x)
